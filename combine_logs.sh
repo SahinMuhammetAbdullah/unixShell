56b9004d391a261664a5a18cb67c5c2c4a284065
Muhammet Abdullah Şahin <m.sahingithub@gmail.com>
2024-08-05 11:36:50 +0300

    Create combine_logs.sh code  is combined these two code: send_syslog.sh and cominde_log.sh

---
+++ combine_logs.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+OUT_LOG_FILE="tmp/combine_logs.log"
+
+if [ ! -f "$OUT_LOG_FILE"
+  touch "$OUT_LOG_FILE"
+fi
+
+LOG_FILES=(
+  "/var/log/auth.log AUTH"
+  "/var/log/syslog SYSLOG"
+  "/var/log/kern.log KERNEL"
+  "/var/log/deamon.log DEAMON"
+  "/var/log/user.log USERLOG"
+  "/var/log/messages MESSAGES"
+)
+for entry in "${LOG_FILES[@]}"; do
+  set -- entry
+  LOG_FILE=$(echo $entry | awk '{print $1}')
+  LOG_TYPE=$(echo $entry | awk '{print $2}')
+  if [ -f "$LOG_FILE" ]; then
+    while IFS= read -r line; do
+      echo "[$LOG_TYPE] $line" >> "$OUT_LOG_FILE"
+    done < "$LOG_FILE"
+    else
+      echo "Log File $LOG_FILE Not Found" >$2
+    fi
+done
+
+echo "Combined Log File Created At $OUT_LOG_FILE"
+
+for clear in "${LOG_FILES[@]}"; do
+  set -- clear
+  CLEAR_LOG_FILE=$(echo $clear | awk '{print $1}')
+  echo "" > "$CLEAR_LOG_FILE"
+done
+
+nc -u 10.10.10.11 514 < /tmp/combined_logs.log
+
+echo "" > "$OUT_LOG_FILE"
